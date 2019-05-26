2af80a63280e3bb7576eb9a2b4965b3838f1a136
Anghy <anghyf27@hotmail.com>
2019-05-26 02:45:04 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+function Index() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Users() {
+  return <h2>Users</h2>
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
+    </Router>
+  )
+}
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'))
