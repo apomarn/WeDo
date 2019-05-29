d59e199b8fb64d88508227d3eed8cb0c530dc7ed
Anghy <anghyf27@hotmail.com>
2019-05-29 10:51:07 -0400

    changed file names

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import Hero from './components/hero'
 import FeatureInformation from './components/feature-information'
 import FeatureDescription from './components/feature-description'
 import React, { Component } from 'react'
-import FooterCTA from './components/footer-cta/footer-cta'
+import FooterCTA from './components/footer-cta'
 
 class Home extends Component {
   render() {
